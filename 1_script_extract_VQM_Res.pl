67aaa5bf7a6f62346a13d30f208f99e283bd57c6
Tran Huyen <tranthithanhhuyenbk@gmail.com>
2018-01-03 15:52:54 +0900

    Add files via upload

--- 1_script_extract_VQM_Res.pl
+++ 1_script_extract_VQM_Res.pl
@@ -4,7 +4,7 @@ my @Res_arr		= (144,240,360,480,720);
 my $No_video 	= 2;
 my $No_qp 		= 2;
 my $No_res 		= 5;
-my $T_e			= 1;
+my $T_e			= 2;
 my $R_file = "VQM_Res.txt";
 open RESULT_FILE, ">$R_file"; 
 printf RESULT_FILE "Video\tQP\tResolution\tAverageVQM\n";
@@ -19,12 +19,12 @@ for (my $cnt_v = 0; $cnt_v < $No_video; $cnt_v++) {
 				my $cmd = 	"ffmpeg -y -i ${Name}/${Name}_qp${QP}_Res${Res}_vbr.mp4 -s 1280x720 -qp 0 -frames 3600 ${Name}/${Name}_qp${QP}_Res${Res}_vbr_Res720.mp4";
 				print "$cmd \n";
 				system $cmd;
-				my $cmd =  "./vqtool -r ${Name}/${Name}_Res720.mp4 -R mp4 -p ${Name}/${Name}_qp${QP}_Res${Res}_vbr_Res720.mp4 -P mp4 -t 120  --vqm  ${Name}_QP${QP}_Res${Res}";
+				my $cmd =  "./vqtool -r ${Name}/${Name}_Res720.mp4 -R mp4 -p ${Name}/${Name}_qp${QP}_Res${Res}_vbr_Res720.mp4 -P mp4 -t $T_e  --vqm  ${Name}_QP${QP}_Res${Res}";
 				print "$cmd \n";
 				system $cmd;
 			}
 			else {
-				my $cmd =  "./vqtool -r ${Name}/${Name}_Res720.mp4 -R mp4 -p ${Name}/${Name}_qp${QP}_Res${Res}_vbr.mp4 -P mp4 -t 120  --vqm  ${Name}_QP${QP}_Res${Res}";
+				my $cmd =  "./vqtool -r ${Name}/${Name}_Res720.mp4 -R mp4 -p ${Name}/${Name}_qp${QP}_Res${Res}_vbr.mp4 -P mp4 -t $T_e  --vqm  ${Name}_QP${QP}_Res${Res}";
 				print "$cmd \n";
 				system $cmd;
 			}
